fa56e99b31e3d1184e2b6de5edebe69b65db8a81
uyuyorum {um} <56808589+alp1x@users.noreply.github.com>
2021-08-18 06:33:35 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,18 +1,18 @@
 -- @uyuyorum qb-idcard {basic} --
 
 QBCore.Functions.CreateUseableItem("id_card", function(source,item)
-    TriggerClientEvent('qb-idcard:open', source)
+    TriggerClientEvent('qb-idcard:open', source, item)
 end)
 
-QBCore.Functions.CreateCallback('qb-idcard:openInformation', function(source, cb)
+QBCore.Functions.CreateCallback('qb-idcard:openInformation', function(source, cb, idcard)
     local src = source
     local xPlayer = QBCore.Functions.GetPlayer(src)
     local inf = {
-         ['name'] = xPlayer.PlayerData.charinfo.firstname .. ' ' .. xPlayer.PlayerData.charinfo.lastname,
-         ['gender'] = xPlayer.PlayerData.charinfo.gender,
-         ['dob'] = xPlayer.PlayerData.charinfo.birthdate,
-         ['nationality'] = xPlayer.PlayerData.charinfo.nationality,
-         ['fingerprint'] = xPlayer.PlayerData.metadata["fingerprint"],
-        }
-         cb(inf)
+        ['name'] = idcard["info"]["lastname"] .. ' ' .. idcard["info"]["firstname"],
+        ['gender'] = idcard["info"]["gender"],
+        ['dob'] = idcard["info"]["birthdate"],
+        ['nationality'] = idcard["info"]["nationality"],
+        ['fingerprint'] = idcard["info"]["fingerprint"],
+    }
+    cb(inf)
 end)
