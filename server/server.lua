01bf52d42bb5e956f15a7dddff83b55f823e3426
DOOM-dev <86206436+DOOM-dev@users.noreply.github.com>
2021-10-18 13:28:29 -0400

    Code cleanup
    Uyuyorum's little fixes to the previous code.

--- server/server.lua
+++ server/server.lua
@@ -17,26 +17,23 @@ QBCore.Functions.CreateUseableItem("lawyerpass", function(source,item)
 end)
 
 
-RegisterServerEvent('qb-idcard:server:showid')
-AddEventHandler('qb-idcard:server:showid',function(source, item, nui)
+RegisterNetEvent('qb-idcard:server:showid',function(source, item, nui)
     local found = false
     local character = QBCore.Functions.GetPlayer(source)
     local PlayerPed = GetPlayerPed(source)
     local PlayerCoords = GetEntityCoords(PlayerPed)
+    local info = {
+        ['name'] = item.info.firstname,
+        ['lastname'] = item.info.lastname,
+        ['gender'] = item.info.gender,
+        ['dob'] = item.info.birthdate,
+        ['nationality'] = item.info.nationality,
+        ['type'] = item.info.type
+    }
     for k, v in pairs(QBCore.Functions.GetPlayers()) do
         local TargetPed = GetPlayerPed(v)
         local dist = #(PlayerCoords - GetEntityCoords(TargetPed))
-        local info = {}
         if dist < 3.0 and PlayerPed ~= TargetPed then
-            info = {
-                ['name'] = item.info.firstname,
-                ['lastname'] = item.info.lastname,
-                ['gender'] = item.info.gender,
-                ['dob'] = item.info.birthdate,
-                ['nationality'] = item.info.nationality,
-                ['type'] = item.info.type,
-                ['fingerprint'] = item.info.fingerprint,
-            }
             TriggerClientEvent('QBCore:Notify', source, "You showed your id to " .. v, "info")
             TriggerClientEvent('qb-idcard:client:open', v, info, nui)
             found = true
@@ -44,16 +41,6 @@ AddEventHandler('qb-idcard:server:showid',function(source, item, nui)
         end
     end
     if not found then
-        local info = {
-            ['name'] = item.info.firstname,
-            ['lastname'] = item.info.lastname,
-            ['gender'] = item.info.gender,
-            ['dob'] = item.info.birthdate,
-            ['nationality'] = item.info.nationality,
-            ['type'] = item.info.type,
-            ['fingerprint'] = item.info.fingerprint,
-        }
         TriggerClientEvent('qb-idcard:client:open', source, info, nui)
-        TriggerClientEvent('QBCore:Notify', source, "There is no-one to show your id to.", "error")
     end
 end)
