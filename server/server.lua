f8316dd54ea1b62191c759ed579fdc1e9b48622b
uyuyorum {um} <56808589+alp1x@users.noreply.github.com>
2023-02-24 11:52:15 +0300

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,21 +1,19 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-local function ShowId(source, item, nui)
+local function ShowId(source, nui)
     local src = source
     local found = false
-    local character = QBCore.Functions.GetPlayer(src)
+    local Player = QBCore.Functions.GetPlayer(src)
     local PlayerPed = GetPlayerPed(src)
     local PlayerCoords = GetEntityCoords(PlayerPed)
     local info = {
-        ['name'] = item.info.firstname,
-        ['lastname'] = item.info.lastname,
-        ['gender'] = item.info.gender,
-        ['dob'] = item.info.birthdate,
-        ['nationality'] = item.info.nationality,
-        ['type'] = item.info.type,
-        ['image'] = item.info.image,
+        ['name'] = Player.PlayerData.charinfo.firstname,
+        ['lastname'] = Player.PlayerData.charinfo.lastname,
+        ['gender'] = Player.PlayerData.charinfo.gender,
+        ['dob'] = Player.PlayerData.charinfo.birthdate,
+        ['nationality'] = Player.PlayerData.charinfo.nationality,
     }
-    for k, v in pairs(QBCore.Functions.GetPlayers()) do
+    for _, v in pairs(QBCore.Functions.GetPlayers()) do
         local TargetPed = GetPlayerPed(v)
         local dist = #(PlayerCoords - GetEntityCoords(TargetPed))
         if dist < 3.0 and PlayerPed ~= TargetPed then
@@ -34,21 +32,21 @@ local function ShowId(source, item, nui)
 end
 
 QBCore.Functions.CreateUseableItem("id_card", function(source,item)
-    ShowId(source, item, 'idcard')
+    ShowId(source, 'idcard')
 end)
 
 QBCore.Functions.CreateUseableItem("driver_license", function(source,item)
-    ShowId(source, item, 'driverlicense')
+    ShowId(source, 'driverlicense')
 end)
 
 QBCore.Functions.CreateUseableItem("weaponlicense", function(source,item)
-    ShowId(source, item, 'weaponlicense')
+    ShowId(source, 'weaponlicense')
 end)
 
 QBCore.Functions.CreateUseableItem("lawyerpass", function(source,item)
-    ShowId(source, item, 'lawyerpass')
+    ShowId(source, 'lawyerpass')
 end)
 
 QBCore.Functions.CreateUseableItem("policecard", function(source,item)
-    ShowId(source, item, 'policecard')
+    ShowId(source, 'policecard')
 end)
