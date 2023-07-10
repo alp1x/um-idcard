0b7a5179dcf4585008a931824415d9ab617107f6
uyuyorum {um} <56808589+alp1x@users.noreply.github.com>
2023-07-10 13:52:49 +0300

    Create ps.lua

---
+++ bridge/inventory/ps.lua
@@ -0,0 +1,14 @@
+if GetResourceState('ps-inventory') ~= 'started' then return end
+
+QBCore = exports['qb-core']:GetCoreObject()
+
+function NewMetaDataLicense(src, itemName)
+    local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
+
+    local newMetaDataItem = Player.Functions.GetItemByName(itemName)
+    if newMetaDataItem then
+        Player.PlayerData.items[newMetaDataItem.slot].info.mugShot = lib.callback.await('um-idcard:client:callBack:getMugShot', src)
+        Player.Functions.SetInventory(Player.PlayerData.items)
+    end
+end
